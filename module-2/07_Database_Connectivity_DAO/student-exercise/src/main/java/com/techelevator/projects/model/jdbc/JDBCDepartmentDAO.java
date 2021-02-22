95a56130db658b404615391cef3e0091406c89d5
Dhakaram Kadarya <dhakaram29@gmail.com>
2021-02-22 01:07:59 -0500

    Homework

--- module-2/07_Database_Connectivity_DAO/student-exercise/src/main/java/com/techelevator/projects/model/jdbc/JDBCDepartmentDAO.java
+++ module-2/07_Database_Connectivity_DAO/student-exercise/src/main/java/com/techelevator/projects/model/jdbc/JDBCDepartmentDAO.java
@@ -6,6 +6,7 @@ import java.util.List;
 import javax.sql.DataSource;
 
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.support.rowset.SqlRowSet;
 
 import com.techelevator.projects.model.Department;
 import com.techelevator.projects.model.DepartmentDAO;
@@ -20,12 +21,27 @@ public class JDBCDepartmentDAO implements DepartmentDAO {
 
 	@Override
 	public List<Department> getAllDepartments() {
-		return new ArrayList<>();
+		List<Department> allDepartments = new ArrayList<Department>();
+		String sqlGetAllDepartments = "SELECT department_id, name " +
+									"FROM department"; 
+		SqlRowSet departmentResults = jdbcTemplate.queryForRowSet(sqlGetAllDepartments);
+		while(departmentResults.next()) {
+			Department theDepartment = mapRowToDepartment(departmentResults);
+			allDepartments.add(theDepartment);
+		}
+		return allDepartments;
+		
 	}
-
 	@Override
 	public List<Department> searchDepartmentsByName(String nameSearch) {
-		return new ArrayList<>();
+		List<Department> departmentList = new ArrayList<Department>();
+		String sqlFindDepartmentByName = "SELECT department_id, name " + "FROM department " + "WHERE name = ?";
+		SqlRowSet departmentResult = jdbcTemplate.queryForRowSet(sqlFindDepartmentByName, nameSearch);
+		while(departmentResult.next()) {
+			Department newDepartment = mapRowToDepartment(departmentResult);
+			departmentList.add(newDepartment);
+		}
+		return departmentList;
 	}
 
 	@Override
@@ -35,12 +51,37 @@ public class JDBCDepartmentDAO implements DepartmentDAO {
 
 	@Override
 	public Department createDepartment(Department newDepartment) {
+		String sqlCreateDepartment = "INSERT into department " +
+				" name VALUES ?";	
+		jdbcTemplate.update(sqlCreateDepartment, newDepartment);
+	
+		return searchDepartmentsByName(newDepartment).get(0);
+		
+	}
+
+	private List<Department> searchDepartmentsByName(Department newDepartment) {
+		// TODO Auto-generated method stub
 		return null;
 	}
 
 	@Override
 	public Department getDepartmentById(Long id) {
-		return null;
+		Department departmentById = new Department();
+		String sqlGetDepartmentById = "SELECT name, department_id " + "From department " + "WHERE department_id = ?";
+		SqlRowSet departmentResult = jdbcTemplate.queryForRowSet(sqlGetDepartmentById, id);
+		while(departmentResult.next()) {
+			departmentById = mapRowToDepartment(departmentResult);
+		}
+		return departmentById;
 	}
+	
 
+	private Department mapRowToDepartment(SqlRowSet results) {
+		Department theDepartment;
+		theDepartment = new Department();
+		theDepartment.setId(results.getLong("department_id"));
+		theDepartment.setName(results.getString("department_id"));
+		return theDepartment;
+				
+	}
 }
--- module-2/07_Database_Connectivity_DAO/student-exercise/src/main/java/com/techelevator/projects/model/jdbc/JDBCProjectDAO.java
+++ module-2/07_Database_Connectivity_DAO/student-exercise/src/main/java/com/techelevator/projects/model/jdbc/JDBCProjectDAO.java
@@ -1,11 +1,13 @@
 package com.techelevator.projects.model.jdbc;
 
+import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 
 import javax.sql.DataSource;
 
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.support.rowset.SqlRowSet;
 
 import com.techelevator.projects.model.Project;
 import com.techelevator.projects.model.ProjectDAO;
@@ -20,17 +22,50 @@ public class JDBCProjectDAO implements ProjectDAO {
 	
 	@Override
 	public List<Project> getAllActiveProjects() {
-		return new ArrayList<>();
+List<Project> activeProjectList = new ArrayList<Project>();
+		
+		String sqlGetAllActiveProjects = "SELECT project_id, name, from_date, to_date " + "FROM project " + "WHERE from_date IS NOT NULL AND (to_date IS NULL OR to_date > ?) ";
+		SqlRowSet results = jdbcTemplate.queryForRowSet(sqlGetAllActiveProjects, LocalDate.now());
+		while(results.next()) {
+			Project resultsProject = mapRowToProject(results);
+			activeProjectList.add(resultsProject);
+			
+		}
+		return activeProjectList;
 	}
+	
 
 	@Override
 	public void removeEmployeeFromProject(Long projectId, Long employeeId) {
 		
+		String sqlRemoveEmployeeFromProject = "DELETE FROM project_employee " + "WHERE project_id = ? AND employee_id = ? ";
+		jdbcTemplate.update(sqlRemoveEmployeeFromProject, projectId, employeeId);
 	}
+	
 
 	@Override
 	public void addEmployeeToProject(Long projectId, Long employeeId) {
 		
+		String sqlAddEmployeeToProject = "INSERT INTO project_employee(project_id, employee_id) " + "VALUES (?, ?) ";
+		jdbcTemplate.update(sqlAddEmployeeToProject, projectId, employeeId);
 	}
+	
+
+	
+
+private Project mapRowToProject(SqlRowSet results) {
+	Project theProject;
+	theProject = new Project();
+	theProject.setId(results.getLong("project_id"));
+	theProject.setName(results.getString("name"));
+	if(results.getDate("from_date") != null){
+		theProject.setStartDate(results.getDate("from_date").toLocalDate());
+	}
+	if(results.getDate("to_date") != null){
+	theProject.setEndDate(results.getDate("to_date").toLocalDate());
+	}
+	return theProject;
+	
+}
 
 }
