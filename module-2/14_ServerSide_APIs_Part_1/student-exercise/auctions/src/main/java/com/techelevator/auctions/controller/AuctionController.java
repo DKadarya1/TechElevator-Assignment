2e01b7dc2d0e2079b8ee5fbbab248b56a5e44949
Dhakaram Kadarya <dhakaram29@gmail.com>
2021-02-28 22:35:33 -0500

    Done H/W

--- module-2/14_ServerSide_APIs_Part_1/student-exercise/auctions/src/main/java/com/techelevator/auctions/controller/AuctionController.java
+++ module-2/14_ServerSide_APIs_Part_1/student-exercise/auctions/src/main/java/com/techelevator/auctions/controller/AuctionController.java
@@ -5,8 +5,10 @@ import com.techelevator.auctions.DAO.MemoryAuctionDAO;
 import com.techelevator.auctions.model.Auction;
 import org.springframework.web.bind.annotation.*;
 
+
 import java.util.List;
 
+import javax.security.auth.login.AccountNotFoundException;
 @RestController
 @RequestMapping("/auctions")
 public class AuctionController {
@@ -16,5 +18,41 @@ public class AuctionController {
     public AuctionController() {
         this.dao = new MemoryAuctionDAO();
     }
+    
+    @RequestMapping( path = "", method = RequestMethod.GET)
+    public List<Auction> list(@RequestParam(defaultValue = "") 
+    String title_like, @RequestParam(defaultValue = "0") double currentBid_lte) {
+    	
+        if( !title_like.equals("") ) {
+            return dao.searchByTitle(title_like);
+        }
+        
+        if(currentBid_lte > 0) {
+            return dao.searchByPrice(currentBid_lte);
+        }
+        if(currentBid_lte > 1 & !title_like.equals("")) {
+            return dao.searchByTitleAndPrice(title_like, currentBid_lte);
+        }
+
+        return dao.list();
+        
+        
+    }
+    
+    
+
+	
+	  @RequestMapping(path = "/{id}", method = RequestMethod.GET) public Auction
+	  get(@PathVariable int id) throws AccountNotFoundException 
+	  { 
+		  return dao.get(id); 
+	  }
+	 
+
+    @RequestMapping( path = "", method = RequestMethod.POST)
+    public Auction create(@RequestBody Auction auction) {
+        return dao.create(auction);
+    }
+
 
 }
